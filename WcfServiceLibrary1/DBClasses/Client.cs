2c7eced2b7993d3cec1341840e09d16af547b1ab
Piotr Reszke <piotr.reszke.1990@gmail.com>
2013-10-11 17:42:03 +0200

    first access class and loggin out added

---
+++ WcfServiceLibrary1/DBClasses/Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bookstore_Service;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Bookstore_Service.DBClasses
+{
+    class Client
+    {
+
+
+        public String login;
+        public String password;
+
+        static public Client getClient(string login){
+               SqlConnection con = new SqlConnection(Bookstore.sqlConnectionString);
+               con.Open();
+                
+
+                SqlDataAdapter loginAdapter = new SqlDataAdapter("SELECT * FROM bookstore.dbo.Client", con);
+                DataSet logins = new DataSet();
+                loginAdapter.Fill(logins,"Client");
+
+                DataTable clientTable = logins.Tables["Client"];
+                DataColumn[] pk = new DataColumn[1];
+                pk[0] = clientTable.Columns["login"];
+                clientTable.PrimaryKey = pk;
+
+                DataRow userRow = clientTable.Rows.Find(login);
+
+                if (userRow == null)
+                {
+                    return null;
+                }
+
+                Client c = new Client();
+                c.login = login;
+                c.password = userRow["password"].ToString();
+
+                return c;
+
+        }
+       
+    }
+}
---
+++ WcfServiceLibrary1/Service.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Data.SqlClient;
+using System.Data;
+using System.Security.Cryptography;
+using System.IdentityModel.Selectors;
+using System.Windows.Forms;
+using Bookstore_Service.DBClasses;
+
+namespace Bookstore_Service
+{
+    [DataContract]
+    public class InternalError
+    {
+        [DataMember]
+        public int Result { get; set; }
+        [DataMember]
+        public string ErrorMessage { get; set; }
+
+        override public string ToString()
+        {
+            return "Result: " + this.Result.ToString() + "\n\nError message: " + this.ErrorMessage.ToString();
+        }
+    }
+
+    [ServiceContract]
+    public interface IBookstore
+    {
+        [OperationContract]
+        [FaultContract(typeof(InternalError))]
+        string Login(string login, string password);
+
+        [OperationContract]
+        int Logout(string login, string sessionToken);
+    }
+
+
+    class Bookstore : IBookstore
+    {
+        static public String sqlConnectionString = "Data Source=DRUADAN-DESKTOP\\SQLEXPRESS; User ID=adm; Password=adm";
+        static Dictionary<String, List<String>> loggedUsers = new Dictionary<string, List<string>>();
+
+        public string Login(string login, string password)
+        {
+            try
+            {
+
+                Client c = Client.getClient(login);
+                if (c == null)
+                {
+                    return "";
+                }
+
+                if (c.password.Equals(password))
+                {
+                    var s = new StringBuilder();
+                    SHA1 hasher = SHA1.Create();
+                    foreach (byte b in hasher.ComputeHash(Encoding.UTF8.GetBytes(login + DateTime.Now.ToString("M/d/yyyy") + new Random().Next())))
+                        s.Append(b.ToString("x2").ToLower());
+
+                    // adding new token to logged in users
+                    if (!loggedUsers.ContainsKey(login))
+                    {
+                        loggedUsers.Add(login, new List<String>());
+                    }
+                    loggedUsers[login].Add(s.ToString());
+
+                    return s.ToString();
+
+                }
+                else
+                {
+                    return "";
+                }
+            }
+            catch (Exception e)
+            {
+                InternalError fault = new InternalError();
+                fault.Result = 1;
+                fault.ErrorMessage = "Logowanie nie powiodło się z powodu wewnętrznego błędu serwera";
+                throw new FaultException<InternalError>(fault, new FaultReason(fault.ErrorMessage));
+            }
+
+            
+        }
+
+
+        public int Logout(string login, string sessionToken)
+        {
+            try
+            {
+                if (!loggedUsers.ContainsKey(login) || loggedUsers[login] == null)
+                {
+                    return 1;
+                }else if( loggedUsers[login].Remove(sessionToken) ){
+                    return 0;
+                }
+
+            }
+            catch (Exception e)
+            {
+                InternalError fault = new InternalError();
+                fault.Result = 1;
+                fault.ErrorMessage = "Wylogowywanie nie powiodło się z powodu wewnętrznego błędu serwera";
+                throw new FaultException<InternalError>(fault, new FaultReason(fault.ErrorMessage));
+            }
+
+            return 1;
+        }
+    }
+}
